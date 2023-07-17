52934cd9b4c55693142a6ab6e4da931c09cdb248
Remon Agayby <60322934+remonagayby@users.noreply.github.com>
2023-07-17 10:49:54 +0300

    change chrome web security to false

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,7 @@ module.exports = defineConfig({
   projectId: '3b45h8',
   e2e: {
     baseUrl: 'https://saucedemo.com/',
+    chromeWebSecurity: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
